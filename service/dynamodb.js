b9e6f1a5ce9942b62fb829d36014523bc221176c
andy3520 <andy.annguyen3520@gmail.com>
2019-10-21 09:04:38 +0700

    Trip table create table, item, scanall, find, drop

--- service/dynamodb.js
+++ service/dynamodb.js
@@ -1,35 +1,31 @@
-require('dotenv').config()
+require("dotenv").config()
 
-const AWS = require('aws-sdk')
-const fs = require('fs')
+const AWS = require("aws-sdk")
+const fs = require("fs")
 
 AWS.config.update({
   region: process.env.REGION,
-  endpoint: process.env.END_POINT,
+  sessionToken: process.env.SESSION_TOKEN,
+  // endpoint: process.env.END_POINT,
   accessKeyId: process.env.ACCESS_KEY,
   secretAccessKey: process.env.SECRET_KEY
 })
 
 const Dynamo = {
   db: new AWS.DynamoDB(),
+  docClient: new AWS.DynamoDB.DocumentClient(),
 
-  // let schema = {
-  //   TableName : "Movies",
-  //   KeySchema: [
-  //       { AttributeName: "year", KeyType: "HASH"},  //Partition key
-  //       { AttributeName: "title", KeyType: "RANGE" }  //Sort key
-  //   ],
-  //   AttributeDefinitions: [
-  //       { AttributeName: "year", AttributeType: "N" },
-  //       { AttributeName: "title", AttributeType: "S" }
-  //   ],
-  //   ProvisionedThroughput: {
-  //       ReadCapacityUnits: 10,
-  //       WriteCapacityUnits: 10
-  //   }
-  // };
-  createTable: function(schema) {
+  createTripTable: function() {
     return new Promise((rs, rj) => {
+      var schema = {
+        TableName: "Trip",
+        KeySchema: [{ AttributeName: "_id", KeyType: "HASH" }],
+        AttributeDefinitions: [{ AttributeName: "_id", AttributeType: "S" }],
+        ProvisionedThroughput: {
+          ReadCapacityUnits: 5,
+          WriteCapacityUnits: 5
+        }
+      }
       this.db.createTable(schema, (err, data) => {
         if (err) return rj(err)
         rs(data)
@@ -37,85 +33,111 @@ const Dynamo = {
     })
   },
 
-  createTripTable: function(params) {
+  createTicketTable: function(params) {},
 
+  dropTripTable: function() {
+    return new Promise((rs, rj) => {
+      let query = {
+        TableName: "Trip"
+      }
+      this.db.deleteTable(query, (err, data) => {
+        if (err) return rj(err)
+        rs(data)
+      })
+    })
   },
 
-  createTicketTable: function(params) {
+  dropTicketTable: function(params) {},
 
+  initTripData: function(params) {},
+
+  initTicketData: function(params) {},
+
+  findAllTrip: function() {
+    return new Promise((rs, rj) => {
+      let query = {
+        TableName: "Trip"
+      }
+      this.docClient.scan(query, (err, data) => {
+        if (err) return rj(err)
+        rs(data)
+      })
+    })
   },
 
-  dropTripTable: function(params) {
+  findTrips: function(from, to, date) {
+    return new Promise((rs, rj) => {
+      let FilterExpression = "",
+        ExpressionAttributeNames = {},
+        ExpressionAttributeValues = {}
+      if (from && from !== "") {
+        FilterExpression += "#from = :from"
+        ExpressionAttributeNames["#from"] = "from"
+        ExpressionAttributeValues[":from"] = from
+      }
+      if (to && to !== "") {
+        FilterExpression += FilterExpression !== "" ? " and " : ""
+        FilterExpression += "#to = :to"
+        ExpressionAttributeNames["#to"] = "to"
+        ExpressionAttributeValues[":to"] = to
+      }
+      if (date && date !== "") {
+        FilterExpression += FilterExpression !== "" ? " and " : ""
+        FilterExpression += "#date = :date"
+        ExpressionAttributeNames["#date"] = "date"
+        ExpressionAttributeValues[":date"] = date
+      }
 
+      let query = {
+        TableName: "Trip",
+        FilterExpression: FilterExpression,
+        ExpressionAttributeNames: ExpressionAttributeNames,
+        ExpressionAttributeValues: ExpressionAttributeValues
+      }
+
+      console.log(query)
+
+      this.docClient.scan(query, (err, data) => {
+        if (err) return rj(err)
+        rs(data)
+      })
+    })
   },
 
-  dropTicketTable: function(params) {
-
-  },
-
-  initTripData: function(params) {
-
-  },
-
-  initTicketData: function(params) {
-
-  },
-
-  findAllTrip: function(params) {
-
-  },
-
-  findTripById: function(params) {
-
-  },
-
-  createTrips: function(params) {
-
-  },
+  createTrips: function(params) {},
 
   createOneTrip: function(params) {
-
+    return new Promise((rs, rj) => {
+      let item = {
+        TableName: "Trip",
+        Item: params
+      }
+      this.docClient.put(item, (err, data) => {
+        if (err) return rj(err)
+        rs(data)
+      })
+    })
   },
 
-  editOneTrip: function(params) {
+  editOneTrip: function(params) {},
 
-  },
+  deleteOneTrip: function(params) {},
 
-  deleteOneTrip: function(params) {
+  deleteAllTrip: function(params) {},
 
-  },
+  findAllTicket: function(params) {},
 
-  deleteAllTrip: function(params) {
+  findTicketById: function(params) {},
 
-  },
+  createTickets: function(params) {},
 
-  findAllTicket: function(params) {
+  createOneTicket: function(params) {},
 
-  },
+  editOneTicket: function(params) {},
 
-  findTicketById: function(params) {
+  deleteOneTicket: function(params) {},
 
-  },
-
-  createTickets: function(params) {
-
-  },
-
-  createOneTicket: function(params) {
-
-  },
-
-  editOneTicket: function(params) {
-
-  },
-
-  deleteOneTicket: function(params) {
-
-  },
-
-  deleteAllTicket: function(params) {
-
-  }
+  deleteAllTicket: function(params) {}
 }
 
 module.exports = Dynamo
--- service/test.js
+++ service/test.js
@@ -1,23 +1,23 @@
-const Dynamo = require('./dynamodb')
+const Dynamo = require("./dynamodb")
 
-let schema = {
-  TableName : "Movies",
-  KeySchema: [
-      { AttributeName: "year", KeyType: "HASH"},  //Partition key
-      { AttributeName: "title", KeyType: "RANGE" }  //Sort key
-  ],
-  AttributeDefinitions: [
-      { AttributeName: "year", AttributeType: "N" },
-      { AttributeName: "title", AttributeType: "S" }
-  ],
-  ProvisionedThroughput: {
-      ReadCapacityUnits: 10,
-      WriteCapacityUnits: 10
-  }
-};
+let trip = {
+  _id: "asdfasfda",
+  from: "An Giang",
+  to: "Bà Rịa–Vũng Tàu",
+  price: 600000,
+  date: "20-10-2019",
+  time: "9:30",
+  quantity: 30
+}
 
-Dynamo.createTable(schema).then((rs) => {
-  console.log(rs)
-}).catch(({message})=>{
-  console.log(message)
-})
+Dynamo.createOneTrip(trip)
+  .then(data => {
+    console.log("create item successful")
+    return Dynamo.findTrips(trip.from, trip.to, trip.date)
+  })
+  .then(data => {
+    console.log(data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
